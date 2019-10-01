3f7c37eed7205e7421ecdbe373f837407a581e52
Felipe Moeller <felipemoeller1921@gmail.com>
2019-10-01 12:22:06 -0300

    accepted answers in phyton

---
+++ 1060.py
@@ -0,0 +1,28 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+
+positivo = 0
+
+if (a > 0):
+    positivo = positivo + 1
+
+if (b > 0):
+    positivo = positivo + 1
+
+if (c > 0):
+    positivo = positivo + 1
+
+if (d > 0):
+    positivo = positivo + 1
+
+if (e > 0):
+    positivo = positivo + 1
+
+if (f > 0):
+    positivo = positivo + 1
+
+print("{} valores positivos".format(positivo))
